845a42768969dfef7466042618d9a2b90ebd5c7c
Bjorn Tore Kopperud <kopperud@protonmail.com>
2022-11-23 15:58:01 +0100

    start docs deploy

---
+++ docs/make.jl
@@ -0,0 +1,9 @@
+using Documenter, Diversification
+
+makedocs(sitename="Diversification")
+
+deploydocs(
+    repo = "github.com/kopperud/Diversification.jl.git",
+)
+
+
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+# Diversification.jl Documentation
+
+lorem ipsum
+
+```@docs
+plottree(x)
+```
+
+
+
+```@docs
+birth_death_shift(model, data)
+```
+
+
