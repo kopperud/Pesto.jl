78ab629105c82055e20ef4462db8aa33ee7d01b9
Bjorn Tore Kopperud <kopperud@protonmail.com>
2023-03-20 17:07:31 +0100

    decompose nshifts

--- src/nshifts.jl
+++ src/nshifts.jl
@@ -1,5 +1,6 @@
 ## number of shifts in the phylogeny
 export compute_nshifts
+export state_shifts
 
 function Amatrix(model, E, K, t)
     Q = -LinearAlgebra.I(K) .* model.η .+ (1 .- LinearAlgebra.I(K)) .* (model.η/(K-1))
@@ -17,11 +18,18 @@ function Pmatrix(model, D, E, t, Δt)
     return(P)
 end
 
-function compute_nshifts(model, data, Ds, Ss; ntimeslices = 500, ape_order = true)
+function state_shifts(model, data; ntimeslices = 500, ape_order = true)
+    Ds, Fs = backwards_forwards_pass(model, data);
+    Ss = ancestral_state_probabilities(data, model, Ds, Fs);
+
+    state_shifts(model, data, Ds, Ss; ntimeslices = ntimeslices, ape_order = ape_order)
+end
+
+function state_shifts(model, data, Ds, Ss; ntimeslices = 500, ape_order = true)
     E = extinction_probability(model, data)
     nbranches = size(data.edges)[1]
     K = length(model.λ)
-    nshifts = zeros(nbranches)
+    nshifts = zeros(nbranches, K, K)
     th = maximum(data.branching_times)
 
     for edge_idx in 1:nbranches
@@ -34,36 +42,50 @@ function compute_nshifts(model, data, Ds, Ss; ntimeslices = 500, ape_order = tru
         ntimes = length(times)
         Δt = times[2] - times[1]
 
-        nshift = 0.0
+        nshift = zeros(K, K)
         for i in 1:(ntimes-1)
             P = Pmatrix(model, Ds[edge_idx], E, times[i], Δt)
             state_prob = Ss[edge_idx](times[i]+Δt)
+            #display(K)
             W = state_prob * ones(K)'
 
             P0 = (1.0 .- LinearAlgebra.I(K)) .* P
             P1 = P0 .* W
 
-            nshift += sum(P1)
+            nshift += P1
         end
-        nshifts[edge_idx] = nshift
+        nshifts[edge_idx,:,:] = nshift
     end
 
     if ape_order
         ## reorder to ape node indices
         ancestors = make_ancestors(data)
 
-        node_nshifts = zeros(maximum(data.edges))
+        node_nshifts = zeros(maximum(data.edges), K, K)
         for i in 1:maximum(data.edges)
             if i == length(data.tiplab)+1
-                node_nshifts[i] = 0.0
+                node_nshifts[i,:,:] .= 0.0
             else
                 edge_idx = ancestors[i]
-                node_val = nshifts[edge_idx]
-                node_nshifts[i] = node_val
+                node_val = nshifts[edge_idx,:,:]
+                node_nshifts[i,:,:] = node_val
             end
         end
         return(node_nshifts)
     else
         return(nshifts)
     end
+end
+
+function compute_nshifts(model, data; ntimeslices = 500, ape_order = true)
+    Ds, Fs = backwards_forwards_pass(model, data);
+    Ss = ancestral_state_probabilities(data, model, Ds, Fs);
+
+    compute_nshifts(model, data, Ds, Ss; ntimeslices = ntimeslices, ape_order = ape_order)
+end
+
+function compute_nshifts(model, data, Ds, Ss; ntimeslices = 500, ape_order = true)
+    nshifts = state_shifts(model, data, Ds, Ss; ntimeslices = ntimeslices, ape_order = ape_order)
+    res = sum(nshifts, dims = 2:3)[:,1,1]
+    return(res)
 end
