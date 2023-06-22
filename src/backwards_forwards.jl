7b6d2ce90513d68e075195dc5c25fd9b08dadef3
Bjorn Tore Kopperud <kopperud@protonmail.com>
2023-06-22 14:53:56 +0200

    clean

--- src/backwards_forwards.jl
+++ src/backwards_forwards.jl
@@ -8,8 +8,6 @@ function backwards_forwards_pass(model, data; verbose = false, alg = OrdinaryDif
 
     E = extinction_probability(model, data)
     D_ends, Ds, sf = postorder(model, data, E; verbose = verbose, alg = alg)
-    #logL = logL_root(model, data)
-    #println("logL: \t", logL)
 
     Fs, F_ends = preorder(model, data, E, D_ends; verbose = verbose, alg = alg)
 
