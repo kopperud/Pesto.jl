c972b7009e5d513cf118d7f3baeaf845654f4243
Bjorn Tore Kopperud <kopperud@protonmail.com>
2023-11-08 16:11:13 +0100

    more plots

--- src/bd_shift/birth_death_shift.jl
+++ src/bd_shift/birth_death_shift.jl
@@ -1,10 +1,9 @@
 ## Wrapper function
 export birth_death_shift
 export plottree
-export bds
 
 @doc raw"""
-    birth_death_shift(model, data[; verbose = false])
+    birth_death_shift(model, data)
 
 Calculates average branch rates under the birth-death-shift model with a finite state space.
 
@@ -25,15 +24,17 @@ model = SSEconstant(λ, μ, η)
 res = birth_death_shift(model, data)
 ```
 """
-function birth_death_shift(model, data; shift_bayes_factor = false, verbose = false)
-    Ds, Fs = backwards_forwards_pass(model, data; verbose = verbose)
+function birth_death_shift(model, data; nshifts = true, shift_bayes_factor = true) 
+    Ds, Fs = backwards_forwards_pass(model, data)
     Ss = ancestral_state_probabilities(data, Ds, Fs)
 
     rates = tree_rates(data, model, Fs, Ss)
 
-    nshift = compute_nshifts(model, data, Ds, Ss; ape_order = false)
-    append!(nshift, 0.0)
-    rates[!,"nshift"] = nshift
+    if nshifts
+        nshift = compute_nshifts(model, data, Ds, Ss; ape_order = false)
+        append!(nshift, 0.0)
+        rates[!,"nshift"] = nshift
+    end
 
     if shift_bayes_factor
         bf = posterior_prior_shift_odds(model,data)
@@ -42,46 +43,6 @@ function birth_death_shift(model, data; shift_bayes_factor = false, verbose = fa
     end
 
     return(rates)
-end
-
-"""
-    bds(model, data[; verbose = false])
-
-Calculates average branch rates under the birth-death-shift model with a finite state space.
-
-Example:
-
-```julia
-using Pesto
-
-phy = readtree(Pesto.path("bears.tre")) 
-ρ = 1.0  
-data = make_SSEdata(phy, "", ρ; include_traits = false) 
-λ = [0.1, 0.2] 
-μ = [0.05, 0.15] 
-
-η = 0.05 
-model = SSEconstant(λ, μ, η)
-
-res = bds(model, data)
-```
-"""
-function bds(model, data; verbose = false)
-    Ds, Fs = backwards_forwards_pass(model, data; verbose = verbose)
-    Ss = ancestral_state_probabilities(data, Ds, Fs)
-
-    rates = tree_rates(data, model, Fs, Ss)
-
-    DataFrames.sort!(rates, :node)
-
-    phy = Dict("edge" => data.edges,
-      "tip.label" => data.tiplab,
-      "Nnode" => length(data.tiplab)-1,
-     "edge.length" => data.branch_lengths)
-
-    out = SSEresult(phy, rates)
-
-    return(out)
 end
 
 """
@@ -94,30 +55,19 @@ res = bds(model, data)
 plottree(res)
 ```
 """
-function plottree(x)
-    phy = x.phy
-    #lambda = x.lambda
-    #mu = x.mu
-    lambda = x.rates[!,"mean_lambda"]
-    mu = x.rates[!,"mean_mu"]
+function plottree(data::SSEdata, rates::DataFrames.DataFrame)
 
-    RCall.@rput lambda
-    RCall.@rput mu
-    RCall.@rput phy
+    RCall.@rput rates
+    p = data
+    RCall.@rput p
+    th = maximum(data.node_depth)
+    RCall.@rput th
 
     RCall.R"""
-    class(phy) <- "phylo"
-    th <- max(ape::node.depth.edgelength(phy))
-    lambda_average <- mean(lambda)
-
-    df1 <- tibble::tibble("node" = 1:max(phy$edge),
-            "Speciation rate" = lambda_average)
-    x <- tidytree::as_tibble(phy)
-
-    phydf <- merge(x, df1, by = "node")
-    td_phy <- tidytree::as.treedata(phydf)
-
-    p1a <- ggtree::ggtree(td_phy, ggplot2::aes(color = `Speciation rate`)) +
+    x <- tidytree::as_tibble(p)
+    td <- tidytree::as.treedata(merge(x, rates, by = "node"))
+    
+    p1a <- ggtree::ggtree(td, ggplot2::aes(color = `mean_lambda`)) +
         ggtree::geom_tiplab(size = 8) +
         ggplot2::theme(legend.position = c(0.2, 0.8)) +
         ggplot2::xlim(c(0.0, th + 10)) 
