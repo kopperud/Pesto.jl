3035e225dd887346166a6cc040caeee31fce35fe
Bjorn Tore Kopperud <kopperud@protonmail.com>
2023-11-08 16:10:30 +0100

    plots

--- src/bd_shift/backwards_forwards.jl
+++ src/bd_shift/backwards_forwards.jl
@@ -1,16 +1,9 @@
 export backwards_forwards_pass
 
-function backwards_forwards_pass(model, data; verbose = false, alg = OrdinaryDiffEq.Tsit5())
-    if verbose
-        println("\t\t")
-        println("Backwards-forwards method:")
-    end
-
+function backwards_forwards_pass(model, data; alg = OrdinaryDiffEq.Tsit5())
     E = extinction_probability(model, data)
-    D_ends, Ds, sf = postorder(model, data, E; verbose = verbose, alg = alg)
-
-    Fs, F_ends = preorder(model, data, E, D_ends; verbose = verbose, alg = alg)
-
+    D_ends, Ds, sf = postorder(model, data, E; alg = alg)
+    Fs, F_ends = preorder(model, data, E, D_ends; alg = alg)
     return(Ds, Fs)
 end
 
