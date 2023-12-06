5985cc5efe8dfd75f08480a01da6bdbc4b3f9963
Bjorn Tore Kopperud <kopperud@protonmail.com>
2023-12-06 14:51:48 +0100

    remove ggtree wrapper

--- src/bd_shift/birth_death_shift.jl
+++ src/bd_shift/birth_death_shift.jl
@@ -45,38 +45,3 @@ function birth_death_shift(model, data; nshifts = true, shift_bayes_factor = tru
 
     return(rates)
 end
-
-"""
-    plottree(x) 
-
-Example:
-
-```julia
-res = bds(model, data)
-plottree(res)
-```
-"""
-function plottree(data::SSEdata, rates::DataFrames.DataFrame)
-
-    RCall.@rput rates
-    p = data
-    RCall.@rput p
-    th = maximum(data.node_depth)
-    RCall.@rput th
-
-    RCall.R"""
-    x <- tidytree::as_tibble(p)
-    td <- tidytree::as.treedata(merge(x, rates, by = "node"))
-    
-    p1a <- ggtree::ggtree(td, ggplot2::aes(color = `mean_lambda`)) +
-        ggtree::geom_tiplab(size = 8) +
-        ggplot2::theme(legend.position = c(0.2, 0.8)) +
-        ggplot2::xlim(c(0.0, th + 10)) 
-    plot(p1a)
-    """
-    return 0;
-end
-
-
-
-
