593daf44b2528908add3f6657f148adfdcdd88a0
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-03-15 14:29:36 +0100

    dummy test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,18 @@ using Pesto
 using Test
 
 @testset "Pesto.jl" begin
+
+    λ = [0.2, 0.1]
+    μ = [0.05, 0.1]
+    η = 0.005
+    model = SSEconstant(λ, μ, η)
+
+    include("../docs/src/primates.jl");
+
+
+    logl = logL_root(model, primates)
+
     # Write your tests here.
-    @test true
+    @test abs(logl - -693.1624874593532) < 0.001
 
 end
