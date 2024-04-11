9104331585688783c9298a82e25165ba33b69af5
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-04-11 14:47:20 +0200

    use safer logl calculation

--- src/bd_shift/logLroot.jl
+++ src/bd_shift/logLroot.jl
@@ -19,18 +19,16 @@ function get_speciation_rates(model::SSEtimevarying, t::Float64)
     return(model.λ(t)) 
 end
 
+
 function logL_root(model::SSE, data::SSEdata)
     E = extinction_probability(model, data)
     #D_ends, sf = postorder_nosave(model, data, E)
-    D_ends, sf = postorder_async(model, data, E)
+    D, sf = postorder_async(model, data, E)
     root_index = length(data.tiplab)+1
     root_age = data.node_depth[root_index]
 
     left_edge, right_edge = findall(data.edges[:,1] .== root_index)
-    D_left = D_ends[left_edge,:]
-    D_right = D_ends[right_edge,:]
 #    λroot = get_speciation_rates(model, root_age)
-    D = D_left .* D_right 
 
     n = number_of_states(model)
     freqs = repeat([1.0 / n], n)
@@ -40,10 +38,10 @@ function logL_root(model::SSE, data::SSEdata)
     # least two lineages must have survived to the present.
     nonextinct = (1.0 .- E(root_age)).^2
 
-    # Why do we divide by (1-E)^2 * λ, and not just (1-E)^2 ?
     D = D ./ nonextinct
     prob = sum(freqs .* D)
     logL = log(prob) + sum(sf)
+    return(logL)
 end
 
 function sselp(η, λ, μ, data)
--- src/bd_shift/postorder_async.jl
+++ src/bd_shift/postorder_async.jl
@@ -1,7 +1,106 @@
-#asd
 export postorder_async
-## async log likelihood calculation
 
+## async log likelihood calculation
+function postorder_async(model::SSE, data::SSEdata, E)
+    ## Pre-compute descendants in hashtable
+    descendants = make_descendants(data)
+
+    K = number_of_states(model)
+
+    ## Postorder traversal: computing the branch probabilities through time
+    nrows = size(data.edges, 1)
+    Ntip = length(data.tiplab)
+    
+    ## Storing the solution at the end of the branch
+    elt = eltype(model)
+
+    ## Storing the scaling factors
+    pD = (model.λ, model.μ, model.η, K, E)
+    u0 = ones(elt, K)
+    tspan = (0.0, 1.0)
+    ode = backward_prob(model)
+    prob = OrdinaryDiffEq.ODEProblem{true}(ode, u0, tspan, pD)
+
+    root_index = Ntip+1
+    left_edge, right_edge = descendants[root_index]
+
+    local sf_left, sf_right
+    local D_left, D_right
+    Threads.@sync begin
+        Threads.@spawn D_left, sf_left = subtree(left_edge, prob, model, data, descendants, Ntip, K, elt)
+        D_right, sf_right =  subtree(right_edge, prob, model, data, descendants, Ntip, K, elt)
+    end
+
+    D = D_left .* D_right
+    sf = sf_left + sf_right
+
+    c = sum(D)
+    D = D ./ c
+    sf = sf_left + sf_right
+
+    if c > 0.0
+        sf += log(c)
+    else
+        sf -= Inf 
+    end
+
+    return(D, sf)
+end
+
+function subtree(edge_index, prob, model, data, descendants, Ntip, K, elt)
+    anc, dec = data.edges[edge_index,:]
+    alg = OrdinaryDiffEq.Tsit5()  
+    node_age = data.node_depth[dec]
+    parent_node_age = data.node_depth[anc]
+    tspan = (node_age, parent_node_age)  
+
+    local sf_left, sf_right
+    if dec <= Ntip
+        u0 = ones(elt, K) .* data.ρ
+        sf_left = 0.0
+        sf_right = 0.0
+    else
+        left_edge, right_edge = descendants[dec]
+        
+        local D_left, D_right
+        Threads.@sync begin ## wait for both subtrees to finish before proceeding
+            ## spawn a new task for the left subtree, so that it can be computed in parallel
+            Threads.@spawn D_left, sf_left = subtree(left_edge, prob, model, data, descendants, Ntip, K, elt)
+            D_right, sf_right = subtree(right_edge, prob, model, data, descendants, Ntip, K, elt)
+        end
+
+       
+        λt = get_speciation_rates(model, node_age) 
+        u0 = D_left .* D_right .* λt
+    end
+
+    sf = sf_left + sf_right
+    if isinf(sf) | isnan(sf) | in(u0, NaN)
+        D = ones(elt, K)
+        sf -= Inf
+    else
+        prob = OrdinaryDiffEq.remake(prob, u0 = u0, tspan = tspan)
+        sol = OrdinaryDiffEq.solve(prob, alg, isoutofdomain = notneg, 
+                                   save_everystep = false, reltol = 1e-3)
+        D = sol.u[end]
+        c = sum(D)
+        D = D ./ c
+
+        if c > 0.0
+            sf += log(c)
+        else
+            sf -= Inf
+        end
+
+        if !(sol.retcode == OrdinaryDiffEq.ReturnCode.Success)
+            sf -= Inf
+        end
+    end
+
+    return(D, sf)
+end
+
+#=
 function postorder_async(model::SSE, data::SSEdata, E)
     ## Pre-compute descendants in hashtable
     descendants = make_descendants(data)
@@ -72,3 +171,5 @@ function subtree!(edge_index, D_ends, sf, prob, model, data, descendants, Ntip, 
         sf[edge_index] += log(c)
     end
 end
+
+=#
