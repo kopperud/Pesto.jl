1ad3c6a3ba2153d191a50822342f836949cfe0ee
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-09-04 11:11:11 +0200

    more descriptive error messages

--- src/errors.jl
+++ src/errors.jl
@@ -5,3 +5,6 @@ struct PolytomyError <: Exception end
 
 struct NegativeBranchError <: Exception end
 
+Base.showerror(io::IO, e::PolytomyError) = print(io, "Your tree contains at least one hard polytomy. Pesto does not work with tress that have polytomies.")
+
+Base.showerror(io::IO, e::NegativeBranchError) = print(io, "Your tree contains at least one branch with negative branch length, preventing Pesto from running.")
