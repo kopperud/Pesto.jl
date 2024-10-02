a07400dbccf25f8a0f08146702f4e61c68e5881c
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-10-02 13:43:40 +0200

    rename model types

--- src/datatypes.jl
+++ src/datatypes.jl
@@ -1,24 +1,39 @@
 export SSEconstant, SSEdata, BDconstant
+export BDSconstant, FBDSconstant
 
-abstract type SSE <: Distributions.ContinuousUnivariateDistribution end
+#abstract type SSE <: Distributions.ContinuousUnivariateDistribution end
+abstract type Model end
 
+#struct BDconstant{T <: Real}#<: Distributions.ContinuousUnivariateDistribution
+struct BDconstant{T <: Real} <: Model
+    λ::T
+    μ::T
+end
 
-struct SSEconstant{T1 <: Real, T2 <: Real} <: SSE
+struct BDSconstant{T1 <: Real, T2 <: Real} <: Model
     λ::Vector{T1}
     μ::Vector{T1}
     η::T2
 end
 
-struct SSEtimevarying <: SSE
+struct BDStimevarying <: Model
     λ::Function
     μ::Function
     η::Function
 end
 
-struct BDconstant{T <: Real} <: Distributions.ContinuousUnivariateDistribution
-    λ::T
-    μ::T
+struct FBDSconstant{T1 <: Real, T2 <: Real, T3 <: Real, T4 <: Real} <: Model
+    λ::Vector{T1}
+    μ::Vector{T2}
+    ψ::T3
+    η::T4
 end
+
+## type aliases to work with the old names
+const SSE = Model
+const SSEconstant = BDSconstant
+const SSEtimevarying = BDStimevarying
+
 
 struct SSEdata
     state_space
