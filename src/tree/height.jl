29dd61ef2299be3f8183bf02dcf7775fde9501f6
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-10-07 17:05:28 +0200

    some tree utility

--- src/tree/height.jl
+++ src/tree/height.jl
@@ -1,18 +1,26 @@
 export treeheight
+export node_depths
 
 function treeheight(tree::Root)
 
-    node_depths = Float64[]
-    time = 0.0
-
-    depths!(node_depths, tree, time)
-
-    height = maximum(node_depths)
+    nd = node_depths(tree)
+    height = maximum(nd)
 
     return(height)
 end
 
+function node_depths(tree::Root)
+    nd = Float64[]
+
+    time = 0.0
+    depths!(nd, tree, time)
+
+    return(nd)
+end
+
 function depths!(nd::Vector{Float64}, node::T, time::Float64) where {T <: InternalNode}
+    push!(nd, time)
+
     for branch in node.children
         child_node = branch.outbounds      
         depths!(nd, child_node, time + branch.time)
@@ -22,3 +30,37 @@ end
 function depths!(nd::Vector{Float64}, node::Tip, time::Float64) 
     push!(nd, time)
 end
+
+###
+function branch_times(tree::Root)
+    t_old = treeheight(tree)
+
+    n_branches = number_of_branches(tree)
+
+    ## t_old and t_young for all branches
+    times = zeros(Float64, n_branches, 2)
+
+    branch_times!(tree, times, t_old)
+
+    return(times)
+end
+
+function branch_times!(node::T, times::Matrix{Float64}, t_old::Float64) where {T <: InternalNode}
+    for branch in node.children 
+        t_young = t_old - branch.time
+        times[branch.index,1] = t_old
+        times[branch.index,2] = t_young
+
+        branch_times!(branch.outbounds, times, t_young)
+    end
+end
+
+function branch_times!(tip::Tip, times::Matrix{Float64}, t_old::Float64)
+    nothing
+end
+
+
+
+
+
+
--- src/tree/utils.jl
+++ src/tree/utils.jl
@@ -33,18 +33,32 @@ end
 
 function get_branches!(node::T, branches) where {T <: InternalNode}
     for branch in node.children
-        #push!(branches, branch)
-        get_branches!(branch, branches)
+        push!(branches, branch)
+        get_branches!(branch.outbounds, branches)
     end
-end
-
-function get_branches!(branch::Branch, branches::Vector{Branch})
-    push!(branches, branch)
-    get_branches!(branch.outbounds, branches)
 end
 
 function get_branches!(node::Tip, branches)
     nothing
 end
 
+export number_of_branches
+
+function number_of_branches(root::Root)
+    n = [0]
+
+    number_of_branches!(root, n)
+    return(n[1])
+end
+
+function number_of_branches!(node::T, n::Vector{Int64}) where {T <: InternalNode}
+    for branch in node.children
+        n[1] += 1
+        number_of_branches!(branch.outbounds, n)
+    end
+end
+
+function number_of_branches!(tip::Tip, n::Vector{Int64})
+    nothing
+end
 
