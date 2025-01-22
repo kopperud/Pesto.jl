4cb7b54f48708226f35d316e29094d7ea4a034fc
Bjorn Tore Kopperud <kopperud@protonmail.com>
2025-01-22 10:47:51 +0100

    clean up branch variance

--- src/bd_shift/branch_variance.jl
+++ src/bd_shift/branch_variance.jl
@@ -1,5 +1,4 @@
 export branch_variance
-export branch_mean
 
 export sp_mean
 export sp_variance
@@ -22,167 +21,7 @@ function sp_variance(model, Ss, branch_index, t)
     return(v)
 end
 
-export brvar
-
-function brvar(
-        model,
-        data,
-        branch_index,
-        num_time_steps,
-    )
-    Ds, Fs = backwards_forwards_pass(model, data)
-    Ss = ancestral_state_probabilities(data, Ds, Fs)
-    E = extinction_probability(model, data)
-
-    D = Ds[branch_index]
-    F = Fs[branch_index]
-    S = Ss[branch_index]
-
-    tmax = maximum(D.t)
-    tmin = minimum(D.t)
-
-    times = collect(range(tmax, tmin; length = num_time_steps))
-    Δt = times[2] - times[1]
-
-    Dt = D(tmax)[:,2]
-    Ft = F(tmax)[:,2]
-    St = ancestral_state_probability(Dt, Ft, tmax)
-
-    means = Float64[]
-    vars = Float64[]
-    prob = Float64[]
-    time_index = 1
-    EX = sum(S(times[time_index]) .* model.λ)
-
-    for state_index in 1:length(model.λ)
-        m = model.λ[state_index] / num_time_steps
-        v = (EX - model.λ[state_index])^2 / num_time_steps
-        p = S(tmax)[state_index]
-        brvar!(model, D, E, S, Δt, times, time_index, state_index, num_time_steps, means, vars, prob, m, v, p)
-    end
-
-    return(means, vars, prob)
-end
-
-function brvar!(
-        model::BhDhModel, 
-        D, 
-        E, 
-        S,
-        Δt::Float64, 
-        times::Vector{Float64}, 
-        time_index::Int64, 
-        state_index::Int64,
-        num_time_steps::Int64, 
-        means::Vector{Float64}, 
-        vars::Vector{Float64}, 
-        prob::Vector{Float64}, 
-        m::Float64,
-        v::Float64,
-        p::Float64)
-    t = times[time_index]
-    P = Pmatrix(model, D, E, t, Δt)
-    EX = sum(S(times[time_index+1]) .* model.λ)
-
-    for i in 1:length(model.λ)
-        m1 = m + model.λ[i] / num_time_steps
-        v1 = v + (EX - model.λ[i])^2 / num_time_steps
-        p1 = p * P[i,state_index] 
-
-        if time_index < (num_time_steps - 1)
-            brvar!(model, D, E, S, Δt, times, time_index+1, i, num_time_steps, means, vars, prob, m1, v1, p1)
-        else
-            push!(means, m1)
-            push!(vars, v1)
-            push!(prob, p1)
-        end
-    end
-end
-
-function branch_variance(model, data, branch_index, n)
-    Ds, Fs = backwards_forwards_pass(model, data)
-    Ss = ancestral_state_probabilities(data, Ds, Fs)
-    E = extinction_probability(model, data)
-
-    D = Ds[branch_index]
-    F = Fs[branch_index]
-    E = extinction_probability(model, data)
-
-    domain = Ds[branch_index].t
-    tmin = minimum(domain)
-    tmax = maximum(domain)
-    n_categories = length(model.λ)
-
-    times = collect(range(tmax, tmin; length = n))
-    
-    var = 0.0
-    Δt = times[2] - times[1]
-
-    for time_idx in 1:(n-1)
-        t = times[time_idx]
-        Dt = D(t)[:,2]
-        Ft = F(t)[:,2]
-        St = ancestral_state_probability(Dt, Ft, t)
-
-        ## Transition probability matrix, given state j
-        P = Pmatrix(model, D, E, t, Δt)
-
-        ## 1st moment
-        ## E[X(t+Δt)]
-        m1 = sum(sum(model.λ[j] * St[j] * P[i,j] for i in 1:4) for j in 1:4)
-        ## 2nd moment
-        ## E[X(t+Δt)^2]
-        m2 = sum(sum(model.λ[j] * model.λ[j] * St[j] * P[i,j] for i in 1:4) for j in 1:4)
-        ## variance of time step t 
-        ## (assuming independence, i.e. 0 covariance)
-        v = m2 - m1^2
-
-        ## add to the total
-        var += v / (n^2)
-    end
-
-    return(var)
-end
-
-export foobaz
-
-function foobaz(model, data, branch_index)
-    Ds, Fs = backwards_forwards_pass(model, data)
-    Ss = ancestral_state_probabilities(data, Ds, Fs)
-    E = extinction_probability(model, data)
-
-    D = Ds[branch_index]
-    F = Fs[branch_index]
-    
-    tmax = maximum(D.t)
-    tmin = minimum(D.t)
-
-    t = tmax
-    Δt = -5.0
-    Dt = D(t)[:,2]
-    Ft = F(t)[:,2]
-
-    times = collect(range(tmax, tmin; length = 100))
-    Δt = times[2] - times[1]
-    println(string("Δt: ", Δt))
-
-    cv1s = Float64[]
-    cv2s = Float64[]
-    for i in 1:99
-        t = times[i]
-        P = Pmatrix(model, D, E, t, Δt)
-        St = ancestral_state_probability(Dt, Ft, t)
-
-        cv1 = sum(sum(model.λ[i] * model.λ[j] * St[j] * P[i,j] for i in 1:4) for j in 1:4)
-        cv2 = sum(sum(model.λ[i] * model.λ[j] * Ss[branch_index](t)[j] * Ss[branch_index](t+Δt)[i] for i in 1:4) for j in 1:4)
-
-        push!(cv1s, cv1)
-        push!(cv2s, cv2)
-    end
-        
-    return(cv1s, cv2s)
-end
-
+#=
 export Amatrix, Pmatrix
 
 function Amatrix(model, E, K, t)
@@ -194,10 +33,144 @@ end
 function Pmatrix(model, D, E, t, Δt)
     K = length(model.λ)
     A = Amatrix(model, E, K, t)
-    Dt = D(t)[:,2]
+    Dt = D(t+Δt)[:,2]
 
     P_unnorm = (LinearAlgebra.I(K) .- Δt .* A) .* (Dt * ones(K)')
     csum = ones(K) * sum(P_unnorm, dims = 1) ## column sum
     P = P_unnorm ./ csum
+    return(P)
+end
+=#
+
+function branch_variance(model, data, c)
+    Ds, Fs = backwards_forwards_pass(model, data)
+
+    vs = zeros(Float64, number_of_branches(data))
+    ProgressMeter.@showprogress for edge_index in eachindex(Ds)
+        D = Ds[edge_index]
+        F = Fs[edge_index]
+
+        v = branch_variance(model, data, D, F, c)
+        vs[edge_index] = v
+    end
+    return(vs)
+end
+
+function branch_variance(
+        model::BhDhModel,
+        data, 
+        branch_index::Int64,
+        c::Int64)
+    Ds, Fs = backwards_forwards_pass(model, data)
+    F = Fs[branch_index]
+    D = Ds[branch_index]
+
+    v = branch_variance(model, data, D, F, c)
+    return(v)
+end
+
+
+function branch_variance(
+        model::BhDhModel, 
+        data, 
+        D::OrdinaryDiffEq.ODESolution, 
+        F::OrdinaryDiffEq.ODESolution, 
+        c::Int64)
+
+    domain = D.t
+    tmin = minimum(domain)
+    tmax = maximum(domain)
+
+    times = collect(range(tmax, tmin; length = c))
+    
+    var_barlambda = 0.0
+    times = collect(range(D.t[end], D.t[1]; length = c))
+
+    for a in 1:c
+        t = times[a]
+        
+        Dt = D(t)[:,2]
+        Ft = F(t)[:,2]
+        St = Dt .* Ft
+        St = St / sum(St)
+
+        first_moment = sum(model.λ .* St)
+        second_moment = sum(model.λ .* model.λ .* St)
+
+        va = second_moment - first_moment^2
+        var_barlambda += (1/c^2) * va
+
+        for b in 2:c
+            if b > a
+                t_b = times[b]
+                var_barlambda += 2*(1/c^2) * covariance(t, t_b, model, D, F)
+            end
+        end
+    end
+    return(var_barlambda)
+end
+
+
+
+function covariance(
+        t1::Float64, 
+        t2::Float64,
+        model::BhDhModel, 
+        D::OrdinaryDiffEq.ODESolution, 
+        F::OrdinaryDiffEq.ODESolution, 
+    )
+    D1 = D(t1)[:,2]
+    D2 = D(t2)[:,2]
+
+    F1 = F(t1)[:,2]
+    F2 = F(t2)[:,2]
+
+    S1 = D1 .* F1
+    S1 = S1 / sum(S1)
+
+    S2 = D2 .* F2
+    S2 = S2 / sum(S2)
+
+    P = Pesto.Pmatrix_precise(model, D, t1, t2)
+
+    E1 = sum(model.λ .* S1) ## expectation at time t1
+    E2 = sum(model.λ .* S2) ## expectation at time t2
+
+    ## covariance
+    co = 0.0
+    for i in 1:4
+        for j in 1:4
+            co += S1[j] * P[i,j] * (model.λ[j] - E1) * (model.λ[i] - E2)
+        end
+    end
+    return(co)
+end
+
+
+function Pmatrix_precise(model, D, t1, t2)
+    K = number_of_states(model)
+    P = zeros(K,K)
+    tspan = (t1, t2)
+    p = (model, K)
+    Et = D(t1)[:,1]
+
+    D2 = D(t2)[:,2]
+
+    for j in 1:K
+        u0 = zeros(K,2)
+        u0[:,1] = Et
+        u0[j,2] = 1.0
+        ode = BhDh_forward_ode
+
+        prob = OrdinaryDiffEq.ODEProblem{true}(ode, u0, tspan, p)
+        sol = OrdinaryDiffEq.solve(prob, OrdinaryDiffEq.Tsit5(), isoutofdomain = notneg, save_everystep = false, reltol = 1e-6)
+        F2 = sol.u[end][:,2] 
+        P[:,j] = F2 .* D2
+    end
+
+    csum = ones(K) * sum(P, dims = 1)
+    P = P ./ csum
+
+
     return(P)
 end
