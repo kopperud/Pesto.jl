3bae01bcea0041706d1ea0880aeaa17afb264b2e
Bjorn Tore Kopperud <kopperud@protonmail.com>
2025-02-27 17:17:58 +0100

    forgot condition parameter for function

--- src/bd_shift/logLroot.jl
+++ src/bd_shift/logLroot.jl
@@ -31,7 +31,7 @@ end
 =#
 
 
-function logL_root(model::Model, data::SSEdata)
+function logL_root(model::Model, data::SSEdata; condition = [:survival, :mrca])
     u, sf = postorder_async(model, data)
 
     E = u[:,1]
@@ -50,11 +50,16 @@ function logL_root(model::Model, data::SSEdata)
     # * that there was a speciation event at the MRCA
     # * that the two lineages subtending from the MRCA 
     #        must have survived until the present
-    λroot = get_speciation_rates(model, root_age)
-    nonextinct = (1.0 .- E).^2
-    condition = λroot .* nonextinct
+    if :survival in condition
+        nonextinct = (1.0 .- E).^2
+        D = D ./ nonextinct
+    end
 
-    D = D ./ condition
+    if :mrca in condition
+        λroot = get_speciation_rates(model, root_age)
+        D = D ./ λroot
+    end
+
     prob = sum(freqs .* D)
     logL = log(prob) + sum(sf)
     return(logL)
